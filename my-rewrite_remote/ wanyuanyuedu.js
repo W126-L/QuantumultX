0312556aeb129672326b91f0673b6cd64cbdb80b
W126-L <126219250+W126-L@users.noreply.github.com>
2023-11-25 04:53:04 +0800

    Update  wanyuanyuedu.js

--- my-rewrite_remote/ wanyuanyuedu.js
+++ my-rewrite_remote/ wanyuanyuedu.js
@@ -1,7 +1,7 @@
 /******************************
 > 原作者：叮当猫（@ios151-Baby）
 [rewrite_local]
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/my-rewrite_remote/20%wanyuanyuedu.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/my-rewrite_remote/wanyuanyuedu.js
 
 [mitm] 
 hostname = buy.itunes.apple.com
