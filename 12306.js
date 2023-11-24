7ed0edf726d2217f67f6d1c20480375d6f3e3d56
W126-L <126219250+W126-L@users.noreply.github.com>
2023-11-25 02:39:08 +0800

    更新 12306.js

--- 12306.js
+++ 12306.js
@@ -10,4 +10,4 @@ hostname = ad.12306.cn
 
 const version = 'V1.0.22';
 
-var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),W126-L={};"0007"==obj.placementNo?(W126-L.materialsList=[{billMaterialsId:"6491",filePath:"W126-L",creativeType:1}],W126-L.advertParam={skipTime:1},W126-L.code="00"):W126-L="G0054"==obj.placementNo?{code:"00",materialsList:[{}]}:{code:"00",message:"无广告返回"},$done({body:JSON.stringify(W126-L)});
