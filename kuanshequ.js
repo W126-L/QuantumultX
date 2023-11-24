a2a86ec9b5845e5287262a1e95e0a5b10b3583ea
W126-L <126219250+W126-L@users.noreply.github.com>
2023-11-25 01:53:45 +0800

    Update kuanshequ.js

--- kuanshequ.js
+++ kuanshequ.js
@@ -2,15 +2,15 @@
 [rewrite_local]
 
 # > 酷安_开屏广告@ddgksf2013
-^https?:\/\/api.coolapk.com\/v6\/main\/init url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/main\/init url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/kuanshequ.js
 # > 酷安_推广广告@ddgksf2013
-^https?:\/\/api.coolapk.com\/v6\/dataList url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/dataList url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/kuanshequ.js
 # > 酷安_首页广告@ddgksf2013
-^https?:\/\/api.coolapk.com\/v6\/main\/indexV8 url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/main\/indexV8 url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/kuanshequ.js
 # > 酷安_评论广告@ddgksf2013
-^https?:\/\/api.coolapk.com\/v6\/feed\/replyList url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/feed\/replyList url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/kuanshequ.js
 # > 酷安_商品推广@ddgksf2013
-^https?:\/\/api.coolapk.com\/v6\/feed\/detail url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/feed\/detail url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/kuanshequ.js
 # > 酷安_屏蔽热词@ddgksf2013
 ^https?:\/\/api\.coolapk\.com\/v6\/search\?.*type=hotSearch url reject-dict
 
