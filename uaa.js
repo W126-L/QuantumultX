06c953f8e107188b4a0497233c5e3b2135234699
W126-L <126219250+W126-L@users.noreply.github.com>
2023-11-27 09:56:10 +0800

    Update uaa.js

--- uaa.js
+++ uaa.js
@@ -6,7 +6,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/(api|m)\.uaa.*\.com url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/uua.js
+^https?:\/\/(api|m)\.uaa.*\.com url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/uaa.js
 
 [mitm]
 hostname = *.uaa*.com
