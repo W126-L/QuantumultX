95d1710b69192b6dbd0f840e23f834f82a57ab36
W126-L <126219250+W126-L@users.noreply.github.com>
2023-11-27 09:31:17 +0800

    Update yijietu2.js

--- yijietu2.js
+++ yijietu2.js
@@ -1,14 +1,13 @@
 /***********************************
 > 应用名称：易截图2
 > 软件版本：1.2.2
-> 脚本作者：ddgksf2013
 > 下载地址：https://apps.apple.com/cn/app/id1633186528
 > 解锁说明：去除水印，解锁高级功能
 
 [rewrite_local]
   
-# > 易截图2☆去除水印（2023-08-02）@ddgksf2013
-^https?:\/\/.*jietu.*com/apiv\d/user url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/YiJieTu2ProCrack.js
+# > 易截图2☆去除水印（2023-08-02）
+^https?:\/\/.*jietu.*com/apiv\d/user url script-response-body https://github.com//W126-L/QuantumultX/main/yijietu2.js
 
 
 [mitm] 
