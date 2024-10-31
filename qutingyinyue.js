9e9f67899a90183193c473093fdb22e807151e9d
W126-L <126219250+W126-L@users.noreply.github.com>
2024-10-31 09:48:36 +0800

    Update qutingyinyue.js

--- qutingyinyue.js
+++ qutingyinyue.js
@@ -4,7 +4,12 @@
 功能说明：屏蔽弹窗/首页横幅
 
 *****************************************
-
+#!name=趣听音乐馆
+#!desc=屏蔽趣听音乐弹窗/首页横幅
+#!author=Wuang
+#!category=⚙️ ▸ 应用屏蔽
+#!icon=https://raw.githubusercontent.com/W126-L/QuantumultX/refs/heads/main/aiyamanya.png
+#
             
 [rewrite_local]
 ^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/qutingyinyue.js
