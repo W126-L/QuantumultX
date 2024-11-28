00a08097faa640d1396e582743e12ccb92c810aa
W126-L <126219250+W126-L@users.noreply.github.com>
2024-11-29 04:34:46 +0800

    更新 yijietu2.js

--- Scripts/yijietu2.js
+++ Scripts/yijietu2.js
@@ -1,10 +1,24 @@
-#!name=易截图2
-#!desc=去除水印，解锁高级功能
-#!author=留存@wuang
-#!category=⚙ ▸ 应用解锁、增强
-#!icon=https://raw.githubusercontent.com/W126-L/QuantumultX/refs/heads/main/yijietu.png
+/***********************************
+
+> 应用名称：易截图2
+> 软件版本：1.2.2
+> 脚本作者：ddgksf2013
+> 微信账号：墨鱼手记
+> 解锁说明：去除水印，解锁高级功能
+> 更新时间：2023-09-05
+> 通知频道：https://t.me/ddgksf2021
+> 贡献投稿：https://t.me/ddgksf2013_bot
+> 问题反馈：ddgksf2013@163.com
+> 特别提醒：如需转载请注明出处，谢谢合作！
+> 特别说明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载售卖
+          ⚠️⚠️
+***********************************/
+
 
 [rewrite_local]
+  
+> 易截图2☆去除水印（2023-08-02）@ddgksf2013
 ^https?:\/\/.*jietu.*com/apiv\d/user url script-response-body https://raw.githubusercontent.com/W126-L/Tool/master/Scripts/yijietu2.js
 
 
