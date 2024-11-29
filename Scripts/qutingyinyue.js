401ef10ef40de48cd39ab8dae3f3cd716ff810cd
W126-L <126219250+W126-L@users.noreply.github.com>
2024-11-29 09:34:02 +0800

    更新 qutingyinyue.js

--- Scripts/qutingyinyue.js
+++ Scripts/qutingyinyue.js
@@ -1,17 +1,20 @@
-#!name=趣听音乐馆
-#!desc=屏蔽趣听音乐弹窗/首页横幅
-#!author=@留存@Wuang
-#!category=⚙️ ▸ 应用屏蔽
-#!icon=https://raw.githubusercontent.com/W126-L/QuantumultX/refs/heads/main/qutingyunyue.png
-#
-*****************************************           
+/****************************************
+
+项目功能：趣听音乐馆
+功能说明：屏蔽弹窗/首页横幅
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+*****************************************
+
+            
 [rewrite_local]
-^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/W126-L/QuantumultX/main/qutingyinyue.js
+^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/quting.js
 
 [mitm]
 hostname = api.bspapp.com, api.next.bspapp.com
 
-****************************************/
 
 
 var body = $response.body.replace(/\"needVerify":\d+/g, '\"needVerify":false').replace(/\"bannerList":\[.+\]/g, '\"bannerList":[]');$done({body});
