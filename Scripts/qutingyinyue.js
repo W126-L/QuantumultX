5ff37b9216792bc7fb5681a45deed2cd0901b957
W126-L <126219250+W126-L@users.noreply.github.com>
2024-11-29 09:39:39 +0800

    更新 qutingyinyue.js

--- Scripts/qutingyinyue.js
+++ Scripts/qutingyinyue.js
@@ -1,13 +1,9 @@
 >项目功能：趣听音乐馆
 >功能说明：屏蔽弹窗/首页横幅
 >脚本作者：chxm1023
-
-
-
-
-            
+        
 [rewrite_local]
-^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/chxm1023/Advertising/main/quting.js
+^https?:\/\/(api|api\.next)\.bspapp\.com\/client$ url script-response-body https://raw.githubusercontent.com/W126-L/Tool/master/Scripts/qutingyinyue.js
 
 [mitm]
 hostname = api.bspapp.com, api.next.bspapp.com
