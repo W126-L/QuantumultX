5af900bae00586ea156183711721817f9d8af22e
W126-L <126219250+W126-L@users.noreply.github.com>
2024-11-29 09:54:44 +0800

    更新 12306.js

--- Scripts/12306.js
+++ Scripts/12306.js
@@ -2,7 +2,7 @@
 > 原作者：墨鱼
 > 使用说明：请在本地添加分流 host, ad.12306.cn, direct
 [rewrite_local]
-^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-analyze-echo-response https://raw.githubusercontent.com/W126-L/QuantumultX/main/12306.js
+^https?:\/\/ad\.12306\.cn\/ad\/ser\/getAdList url script-analyze-echo-response https://raw.githubusercontent.com/W126-L/Tool/master/Scripts/12306.js
 
 [mitm]
 hostname = ad.12306.cn
