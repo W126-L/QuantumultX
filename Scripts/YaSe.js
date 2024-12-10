49a6bde700485975fcb7f897f7dda77044036d6b
W126-L <126219250+W126-L@users.noreply.github.com>
2024-12-11 05:45:59 +0800

    Create YaSe.js

---
+++ Scripts/YaSe.js
@@ -0,0 +1,72 @@
+/*******************************
+
+脚本功能：亚瑟浏览器——解锁订阅
+下载地址：https://is.gd/oVW6D9
+软件版本：11.6（貌似11.4更稳妥）
+脚本作者：彭于晏💞
+更新时间：2023-9-12
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/W126-L/Tool/master/Scripts/yase.js
+
+
+[MITM]
+
+hostname = buy.itunes.apple.com
+
+********************************/
+
+
+
+var baby = { 
+    "receipt": { 
+      "receipt_type": "Production", 
+      "bundle_id": "https://t.me/ios151", 
+      "in_app": [ 
+        { 
+          "quantity": "1", 
+          "purchase_date_ms": "666666666666666", 
+          "transaction_id": "666666666666666", 
+          "original_transaction_id": "666666666666666", 
+          "product_id": "com.ys.pro", 
+          "in_app_ownership_type": "PURCHASED", 
+          "original_purchase_date_ms": "666666666666666" 
+        } 
+      ], 
+      "application_version": "6", 
+      "original_purchase_date_ms": "666666666666666", 
+      "original_application_version": "6" 
+    }, 
+    "environment": "Production", 
+    "pending_renewal_info": [ 
+      { 
+        "product_id": "com.ys.pro", 
+        "auto_renew_status": "1" 
+      } 
+    ], 
+    "status": 0, 
+    "latest_receipt_info": [ 
+      { 
+        "quantity": "1", 
+        "purchase_date_ms": "666666666666666", 
+        "expires_date": "8888-08-08 08:08:08 Etc\/GMT", 
+        "expires_date_pst": "8888-08-08 08:08:08 America\/Los_Angeles", 
+        "is_in_intro_offer_period": "false", 
+        "transaction_id": "666666666666666", 
+        "is_trial_period": "false", 
+        "original_transaction_id": "666666666666666", 
+        "product_id": "com.ys.pro", 
+        "original_purchase_date_ms": "666666666666666", 
+        "expires_date_ms": "148204937166000" 
+      } 
+    ] 
+};
+$done({ body: JSON.stringify(baby) });
